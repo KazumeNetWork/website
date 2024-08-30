6126e311f6243ac4b0315741d509ebb2d5805d36
yukkuriremiria <98326743+yukkuriremiria@users.noreply.github.com>
2024-08-31 05:52:52 +0900

    Update WebSite

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+        document.addEventListener('click', function() {
+            var audio = document.getElementById('audio');
+            if (audio.paused) {
+                audio.volume = 0.5;
+                audio.play();
+            }
+        });
+        
